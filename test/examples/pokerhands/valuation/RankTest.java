3c56ddb4b52e01b34db9279ca747b2610b9847c4
Ralf Pannemans <ralf@Rouge.fritz.box>
2016-07-10 12:07:53 +0200

    Add missing Rank.toString() test

--- test/examples/pokerhands/valuation/RankTest.java
+++ test/examples/pokerhands/valuation/RankTest.java
@@ -15,5 +15,19 @@ public class RankTest {
 	public void testNoneIsLowest() {
 		assertEquals(0, Rank.NONE.ordinal());
 	}
+	
+	@Test
+	public void testToString() {
+		assertEquals("Flush", Rank.FLUSH.toString());
+		assertEquals("Four of a kind", Rank.FOUR_KIND.toString());
+		assertEquals("Full House", Rank.FULL_HOUSE.toString());
+		assertEquals("No ranking", Rank.NONE.toString());
+		assertEquals("Pair", Rank.PAIR.toString());
+		assertEquals("Straight", Rank.STRAIGHT.toString());
+		assertEquals("Straight Flush", Rank.STRAIGHT_FLUSH.toString());
+		assertEquals("Three of a kind", Rank.THREE_KIND.toString());
+		assertEquals("Two Pairs", Rank.TWO_PAIRS.toString());
+	}
+	
 
 }
